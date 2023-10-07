42d22881b70e8b016ce8dc906a001de1fef011bf
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-10-07 20:55:46 +0300

    Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnRadius = 10;
+    public int spawnCount = 10;
+    public Color colour;
+
+    void Awake()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 pos = transform.position + Random.insideUnitSphere * spawnRadius;
+            GameObject boid = Instantiate(prefab);
+            boid.transform.position = pos;
+            boid.transform.forward = Random.insideUnitSphere;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(colour.r, colour.g, colour.b, 0.3f);
+        Gizmos.DrawSphere(transform.position, spawnRadius);
+    }
+
+}
